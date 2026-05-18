244c2b7da05943433430d84f6b392fcbb0715ab3
suhanighodake <ghodakesuhani@gmail.com>
2026-05-18 13:28:15 +0530

    creating home page

--- fruniture/src/App.tsx
+++ fruniture/src/App.tsx
@@ -1,7 +1,15 @@
-import Home from "./pages/Home"
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/Home";
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
