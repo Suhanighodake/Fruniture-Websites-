1ef4669683089cc7848e838d699a5978809dc859
Rushijadhav1 <rushikesh847@gmail.com>
2026-05-18 13:29:16 +0530

    fix some changes

--- fruniture/src/main.tsx
+++ fruniture/src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// @ts-ignore: CSS side-effect import declaration not found
-import "./index.css"
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// @ts-ignore
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")!
+).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
